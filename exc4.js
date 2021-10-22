4098343489b533f96851330219ed70992cc5336f
grayzerguy <grayzerg@gmail.com>
2021-10-22 17:25:09 +0300

    fixbug

--- exc4.js
+++ exc4.js
@@ -4,12 +4,13 @@ const inputText1 = document.querySelector("#text");
 const outputText1 = document.querySelector("#output");
 
 inputText1.addEventListener("keypress", function (event) {
-  for (let char of arryOfchars) {
-    if (event.key !== char) {
-      outputText1.innerHTML = "";
-    } else {
+  for (let i of arryOfchars) {
+    if (event.key === i) {
       event.preventDefault();
-      outputText1.innerHTML = "You can't use this character";
+      outputText1.textContent = `You  can not use  ${i}`;
     }
   }
 });
+// if (event.keypress===arryOfchars) {
+//   event.preventDefault();
+//   outputText1.innerHTML = "You can't use this character";
