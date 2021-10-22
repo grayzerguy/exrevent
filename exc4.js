a91d75e1b816af9fe8723227ab46997e1d4f0e5f
grayzerguy <grayzerg@gmail.com>
2021-10-22 17:54:08 +0300

    פתרון נוסף

--- exc4.js
+++ exc4.js
@@ -3,14 +3,20 @@ const arryOfchars = ["!", "@", "#", "$", "%"];
 const inputText1 = document.querySelector("#text");
 const outputText1 = document.querySelector("#output");
 
+// inputText1.addEventListener("keypress", function (event) {
+//   for (let i of arryOfchars) {
+//     if (event.key === i) {
+//       event.preventDefault();
+//       outputText1.textContent = `You  can not use  ${i}`;
+//     }
+//   }
+// });
+
 inputText1.addEventListener("keypress", function (event) {
-  for (let i of arryOfchars) {
-    if (event.key === i) {
+  array.forEach(element, function (element) {
+    if (event.key === element) {
       event.preventDefault();
-      outputText1.textContent = `You  can not use  ${i}`;
+      outputText1.textContent = `You can't use this character`;
     }
-  }
+  });
 });
-// if (event.keypress===arryOfchars) {
-//   event.preventDefault();
-//   outputText1.innerHTML = "You can't use this character";
